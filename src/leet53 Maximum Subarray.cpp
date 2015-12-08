b6b7d8764c3ce17dcd3f1d8ac1ac4b43a8236661
Hys2015 <markheng2014@gmail.com>
2015-12-08 13:07:46 +0800

    Update leet53 Maximum Subarray.cpp

--- src/leet53 Maximum Subarray.cpp
+++ src/leet53 Maximum Subarray.cpp
@@ -1,9 +1,12 @@
 /*
+https://leetcode.com/problems/maximum-subarray/
+
 Find the contiguous subarray within an array (containing at least one number) which has the largest sum.
 
 For example, given the array [−2,1,−3,4,−1,2,1,−5,4],
 the contiguous subarray [4,−1,2,1] has the largest sum = 6.
 
+有负数即产生截断，最大值即需重新计数
 */
 
 
